18092eb0edb17606c5f8dc0dceaa194b859e4596
Larysa Holtseva <holtseva@seznam.cz>
2022-08-14 14:16:30 +0200

    add changes

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './js/slick-slider';
+import './js/modal-window';
+import './js/mob-menu';
+import './js/smooth-scroll';
+import './js/details';
+import './js/scroll-to-top';
---
+++ src/js/modal-window.js
@@ -0,0 +1,31 @@
+// CONTACTS MODAL
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
+
+// HEADER MODAL
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-opn]'),
+    closeModalBtn: document.querySelector('[data-modal-cls]'),
+    modal: document.querySelector('[data-modal-wind]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
